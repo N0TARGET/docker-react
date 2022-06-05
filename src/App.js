6aff93a4943e456adfa34ae9dd6c5d0ca16c0d24
Alekz <pr@yandex.ru>
2022-06-05 12:19:03 +0200

    sveta konf

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Sveta Konfeta :)
+          Sveta KonfetaAAAAAAAAAAAAAAAAAA
         </a>
       </header>
     </div>
